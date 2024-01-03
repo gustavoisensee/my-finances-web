447debab03b3d3356ad10979febd91ca5693fb2a
Gustavo Isensee <gustavo.isensee@gmail.com>
2024-01-03 22:53:29 +0100

    fix: remove category table column

--- src/components/category/CategoryTable.tsx
+++ src/components/category/CategoryTable.tsx
@@ -26,7 +26,6 @@ const CategoryTable = ({ data }: Props) => (
               <tr>
                 <th scope='col' className='px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase'>Name</th>
                 <th scope='col' className='px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase'>Owner</th>
-                <th scope='col' className='px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase'>Action</th>
               </tr>
             </thead>
             <tbody className='divide-y divide-gray-200'>
@@ -34,14 +33,6 @@ const CategoryTable = ({ data }: Props) => (
                 <tr key={i}>
                   <td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800'>{d.name}</td>
                   <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-800'>{isAdmin(d.userId) ? <AdminBadge /> : <YouBadge />}</td>
-                  <td className='px-6 py-4 whitespace-nowrap text-end text-sm font-medium'>
-                    {!isAdmin(d.userId) &&
-                      <button
-                        type='button'
-                        className='inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none'
-                        onClick={() => alert('Work in progress')}
-                      >Delete</button>}
-                  </td>
                 </tr>
               ))}
             </tbody>
